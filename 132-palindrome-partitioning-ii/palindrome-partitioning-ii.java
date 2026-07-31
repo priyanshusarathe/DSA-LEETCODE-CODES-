b3f4c0543d7737ba364701c89051c7e374e985dd
priyanshusarathe03 <priyanshusrarathe@gmail.com>
2026-07-31 17:55:52 +0530

    Time: 2495 ms (18.63%) | Memory: 42.7 MB (87.85%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,39 +1,38 @@
 class Solution {
-    public boolean isPalindrome(int i, int j,String s){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+
+    public boolean isPalindrome(int i, int j, String s) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
                 return false;
-            }
             i++;
             j--;
         }
-    return true;
+        return true;
     }
-    public int helper(int i,int n, String s,int [] dp){
-        if(i==n){
-         return 0;
-        }
-        int minCost= Integer.MAX_VALUE;
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        for(int j=i;j<n;j++){
-            if(isPalindrome(i,j,s)){
-               int cost= 1 + helper(j+1,n,s,dp);
-               minCost = Math.min(cost,minCost);
-            }
-  
-        }
-            return dp[i] = minCost;
-    }
-    public int minCut(String s) {
-        if(s.length()==1){
-            return 0;
-        }
-        int n = s.length();
-        int [] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return helper(0,n,s,dp)-1;
 
+    public int minCut(String s) {
+
+        int n = s.length();
+
+        int[] dp = new int[n + 1];
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int minCost = Integer.MAX_VALUE;
+
+            for (int j = i; j < n; j++) {
+
+                if (isPalindrome(i, j, s)) {
+
+                    int cost = 1 + dp[j + 1];
+                    minCost = Math.min(minCost, cost);
+                }
+            }
+
+            dp[i] = minCost;
+        }
+
+        return dp[0] - 1;
     }
 }
