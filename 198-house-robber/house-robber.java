d3e3470c83952eaa147a24f16f2f4f38d3310f7f
priyanshusarathe03 <priyanshusrarathe@gmail.com>
2026-08-20 09:22:33 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.53%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,28 @@
 class Solution {
-    public int helper(int i, int[] nums,int [] dp){
-        if(i<0){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int skip = helper(i-1,nums,dp);
-        int take = nums[i]+helper(i-2,nums,dp);
-        return dp[i]= Math.max(skip,take);
-    }
+    // public int helper(int i, int[] nums,int [] dp){
+    //     if(i<0){
+    //         return 0;
+    //     }
+    //     if(dp[i]!=-1){
+    //         return dp[i];
+    //     }
+    //     int skip = helper(i-1,nums,dp);
+    //     int take = nums[i]+helper(i-2,nums,dp);
+    //     return dp[i]= Math.max(skip,take);
+    // } memoization
     public int rob(int[] nums) {
+        
         int [] dp  = new int[nums.length];
         Arrays.fill(dp,-1);
-      return  helper(nums.length-1,nums,dp);
+        dp[0]=nums[0];
+        if(nums.length==1){
+            return dp[0];
+        }
+        dp[1]=Math.max(nums[0],nums[1]);
+        
+        for(int i=2;i<nums.length;i++){
+              dp[i]= Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+      return  dp[nums.length-1];
     }
 }
